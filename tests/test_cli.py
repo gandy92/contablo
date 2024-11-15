2d8aafd23341a93120cc02d3610accc3a4bb3572
gandy92 <gandy92@googlemail.com>
2024-11-15 16:42:42 +0100

    test: Add cli tests

---
+++ tests/test_cli.py
@@ -0,0 +1,47 @@
+from glob import glob
+import json
+from pytest import TempdirFactory
+from click.testing import CliRunner
+
+from contablo.cli import cli
+
+
+def test_mk_schema():
+    runner = CliRunner()
+    base = "test-schema-"
+    schema_name = "ImportSpec"
+    with runner.isolated_filesystem():
+        result = runner.invoke(cli, ["mk-schema", "-o", base, schema_name])
+        assert result.exit_code == 0
+
+        with open(f"{base}{schema_name.lower()}.json") as f:
+            schema = json.load(f)
+        assert "$defs" in schema
+
+
+def test_mk_import_template():
+    runner = CliRunner()
+
+    files = [
+        "example-4.csv",
+        "fieldspec-banking.json",
+    ]
+    file_data = {}
+    for filename in files:
+        with open(f"tests/{filename}", "r") as f:
+            file_data[filename] = f.readlines()
+
+    with runner.isolated_filesystem():
+        for filename in files:
+            with open(f"{filename}", "w") as f:
+                f.writelines(file_data[filename])
+
+        result = runner.invoke(
+            cli, ["mk-import-tmpl", "-t", "fieldspec-banking.json", "-o", "test_template", "example-4.csv"]
+        )
+        assert result.exit_code == 0
+        print(result.output)
+
+        out_files = glob("test_template*.json")
+        print(out_files)
+        assert len(out_files) == 1
